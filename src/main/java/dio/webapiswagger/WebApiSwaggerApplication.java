dff421149426ca894c44b70f6f324d25f513d182
Ruan Moreira <ruaanmoreira2508@gmail.com>
2023-01-31 10:10:09 -0300

    "First Commit"

---
+++ src/main/java/dio/webapiswagger/WebApiSwaggerApplication.java
@@ -0,0 +1,13 @@
+package dio.webapiswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dio/webapiswagger/model/Usuario.java
@@ -0,0 +1,7 @@
+package dio.webapiswagger.model;
+
+public class Usuario {
+    private String login;
+    private String password;
+    public Usuario() {}
+}
