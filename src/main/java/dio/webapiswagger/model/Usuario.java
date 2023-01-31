2de22a5a862a542f46fc0ed89d3bf3662f12fab2
Ruan Moreira <ruaanmoreira2508@gmail.com>
2023-01-31 10:56:14 -0300

    "Fake Repository & Rotes"

--- src/main/java/dio/webapiswagger/model/Usuario.java
+++ src/main/java/dio/webapiswagger/model/Usuario.java
@@ -4,4 +4,33 @@ public class Usuario {
     private String login;
     private String password;
     public Usuario() {}
+
+    public Usuario(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/main/java/dio/webapiswagger/repository/UsuarioRepository.java
@@ -0,0 +1,38 @@
+package dio.webapiswagger.repository;
+
+import dio.webapiswagger.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+    public void save(Usuario usuario){
+        System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void update(Usuario usuario){
+        System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void delete(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+    public List<Usuario> findAll(){
+        System.out.println("LIST - Listando os usários do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Ruan","password"));
+        usuarios.add(new Usuario("frank","masterpass"));
+        return usuarios;
+    }
+    public Usuario findById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
+    }
+
+    public Usuario findByUsername(String username) {
+        return new Usuario( username, "password");
+    }
+}
